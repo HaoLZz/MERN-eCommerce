e52293a8c3b9997bcf301557bfc0afabfb7cf99a
Haoliang Zhang <hlzhang26@outlook.com>
2022-07-08 17:35:59 -0600

    Add fetchProducts thunk

--- frontend/src/features/products/productsSlice.js
+++ frontend/src/features/products/productsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   data: [],
@@ -6,10 +7,32 @@ const initialState = {
   error: null,
 };
 
+export const fetchProducts = createAsyncThunk(
+  'products/fetchProducts',
+  async () => {
+    const response = await axios.get('/api/products');
+    return response.data;
+  },
+);
+
 const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.data = state.data.concat(action.payload);
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default productsSlice;
