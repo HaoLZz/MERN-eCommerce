88003f41a1076c1a214a41ae8df9497b1c23a7f7
Haoliang Zhang <hlzhang26@outlook.com>
2022-07-14 13:27:13 -0600

    Create UI elements for displaying products info in CartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,94 @@
 import React from 'react';
+import { Link, Outlet, useParams } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+  ListGroupItem,
+} from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import Message from '../components/Message';
 
 const CartScreen = () => {
-  return <div>Shopping Cart</div>;
+  const { id, qty } = useParams();
+
+  const { cartItems } = useSelector((state) => state.cart);
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to="/">Go Back</Link>{' '}
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroupItem key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="input"
+                      type="number"
+                      min="1"
+                      max={item.countInStock}
+                      value={item.qty}
+                      onChange={(f) => f}
+                    ></Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button type="button" variant="ligth" onClick={(f) => f}>
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              <span>
+                $
+                {cartItems
+                  .reduce((acc, item) => acc + item.qty * item.price, 0)
+                  .toFixed(2)}
+              </span>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={(f) => f}
+              >
+                Proceed to Checkout
+              </Button>
+            </ListGroupItem>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartScreen;
